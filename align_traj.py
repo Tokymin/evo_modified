4e9f292ccf43651fb4050a5b70388907dbad88b0
Tokymin <466350259@qq.com>
2023-04-23 11:49:29 +0800

    init

---
+++ align_traj.py
@@ -0,0 +1,18 @@
+# 对齐两条轨迹并把对齐后的结果写入csv文件
+# # 调用evo计算APE等
+from evo import entry_points
+from evo import main_rpe
+import argcomplete
+
+model_prefix="Ours"
+main_rpe.est_file = r"source/Ours.kitti"
+main_rpe.ref_file = "source/Ground_Truth.kitti"
+main_rpe.model_prefix = model_prefix
+
+main_rpe.rpe_metric_csv_path = r"save_metric/RPE_metric" + model_prefix + ".csv"  # 保存csv的文件
+main_rpe.ape_metric_csv_path = r"save_metric/APE_metric" + model_prefix + ".csv"
+main_rpe.modified_csv_path = r"save_align/save_align_traj" + model_prefix + ".csv"
+
+parser = main_rpe.parser()
+argcomplete.autocomplete(parser)
+entry_points.launch(main_rpe, parser)
